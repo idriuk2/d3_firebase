0b940fa19ecb0676cba7e3e718e8c32d59bfce59
Ivan Driuk <idriuk@zaraffasoft.com>
2019-09-12 10:14:47 +0300

    update pattern, firestore config

--- index.js
+++ index.js
@@ -1,11 +1,83 @@
-const data = [
-  {width: 200, height: 100, fill: 'red'}
-]
+// select the svg container first
+const svg = d3.select('.canvas')
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
 
-const svg = d3.select('svg')
+// create margins & dimensions
+const margin = {top: 20, right: 20, bottom: 100, left: 100};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
 
-const rect = svg.select('rect')
-  .data(data)
-  .attr('width', function(d, i, n) { return d.width})
-  .attr('height', function(d) { return d.height })
-  .attr('fill', function(d) { return d.fill })
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+// create axes groups
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`)
+
+xAxisGroup.selectAll('text')
+  .attr('fill', 'orange')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end');
+
+const yAxisGroup = graph.append('g');
+
+const y = d3.scaleLinear()
+    .range([graphHeight, 0]);
+
+const x = d3.scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
+// create & call axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
+
+// the update function
+const update = (data) => {
+
+  // join the data to circs
+  const rects = graph.selectAll('rect')
+    .data(data);
+
+  // update the domains
+  y.domain([0, d3.max(data, d => d.orders)]);
+  x.domain(data.map(item => item.name));
+
+  // add attrs to rects already in the DOM
+  rects.attr('width', x.bandwidth)
+    .attr("height", d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
+
+  // append the enter selection to the DOM
+  rects.enter()
+    .append('rect')
+      .attr('width', x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name))
+      .attr('y', d => y(d.orders));
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+};
+
+db.collection('dishes').get().then(res => {
+
+  var data = [];
+  res.docs.forEach(doc => {
+    data.push(doc.data());
+  });
+
+  update(data);
+  
+});
