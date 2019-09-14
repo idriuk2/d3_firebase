7d5cfb07ce658d32c22ee6630e2f5e08607c1dc8
Ivan Driuk <veytarn@gmail.com>
2019-09-14 10:07:23 +0300

    arc tween, this, _current, interpolation obj

--- graph.js
+++ graph.js
@@ -20,28 +20,51 @@ const arcPath = d3.arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-// ordianl colour scale
+// ordinal colour scale
 const colour = d3.scaleOrdinal(d3["schemeSet3"]);
+
+// legend setup
+const legendGroup = svg.append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`)
+
+const legend = d3.legendColor()
+  .shape('path', d3.symbol().type(d3.symbolCircle)())
+  .shapePadding(10)
+  .scale(colour)
 
 // update function
 const update = (data) => {
 
   // update colour scale domain
   colour.domain(data.map(d => d.name));
+
+  // update legend
+  legendGroup.call(legend);
+  legendGroup.selectAll('text').attr('fill', 'white');
   
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll('path')
     .data(pie(data));
 
-  console.log(paths);
+  // handle the exit selection 
+  paths.exit()
+    .transition().duration(750)
+    .attrTween("d", arcTweenExit)
+    .remove();
+
+  // handle the current DOM path updates
+  paths.transition().duration(750)
+    .attrTween("d", arcTweenUpdate);
 
   paths.enter()
     .append('path')
       .attr('class', 'arc')
-      .attr('d', arcPath)
       .attr('stroke', '#fff')
       .attr('stroke-width', 3)
-      .attr('fill', d => colour(d.data.name));
+      .attr('d', arcPath)
+      .attr('fill', d => colour(d.data.name))
+      .each(function(d){ this._current = d })
+      .transition().duration(750).attrTween("d", arcTweenEnter);
 
 };
 
@@ -75,3 +98,35 @@ db.collection('expenses').orderBy('cost').onSnapshot(res => {
   update(data);
 
 });
+
+const arcTweenEnter = (d) => {
+  var i = d3.interpolate(d.endAngle-0.1, d.startAngle);
+
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+
+const arcTweenExit = (d) => {
+  var i = d3.interpolate(d.startAngle, d.endAngle);
+
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
+
+// use function keyword to allow use of 'this'
+function arcTweenUpdate(d) {
+  console.log(this._current, d);
+  // interpolate between the two objects
+  var i = d3.interpolate(this._current, d);
+  // update the current prop with new updated data
+  this._current = i(1);
+
+  return function(t) {
+    // i(t) returns a value of d (data object) which we pass to arcPath
+    return arcPath(i(t));
+  };
+};
