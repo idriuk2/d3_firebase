9b96df58b004146591bc7dba555750c2ee2dc74a
Ivan Driuk <idriuk@zaraffasoft.com>
2019-09-25 10:13:23 +0300

    text outside arc from left to right

--- arc_text/index.js
+++ arc_text/index.js
@@ -1,11 +1,53 @@
-const svg = d3.select('.canvas')
+// const svg = d3.select('.canvas')
+//   .append('svg')
+//   .attr('width', 400)
+//   .attr('height', 400);
+
+// svg.append("defs").append("path")
+//     .attr("id", "arc")
+//     .attr("d", circlePath(100, 100, 100, 0, 90));
+
+// var thing = svg.append("g")
+//     .attr("id", "thing")
+//     .style("fill", "navy");
+
+// thing.append("text")
+//     .style("font-size", "20px")
+//   .append("textPath")
+//     .attr("xlink:href", "#arc")
+//     .text("Wavy text is the gimmick for many years to come (d3)");
+
+// thing.append("use")
+//     .attr("xlink:href", "#arc")
+//     .style("stroke", "black")
+//     .style("fill", "none");
+
+//   function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+//     var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+//     return {
+//       x: centerX + radius * Math.cos(angleInRadians),
+//       y: centerY + radius * Math.sin(angleInRadians),
+//     };
+//   }
+
+//   function circlePath(x, y, radius, startAngle, endAngle) {
+//     var start = polarToCartesian(x, y, radius, endAngle * 0.9999);
+//     var end = polarToCartesian(x, y, radius, startAngle);
+//     var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+//     var d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y];
+//     return d.join(' ');
+//   }
+
+/* text outside circle from left to right */
+
+  const svg = d3.select('.canvas')
   .append('svg')
-  .attr('width', 400)
-  .attr('height', 400);
+  .attr('width', 600)
+  .attr('height', 600);
 
 svg.append("defs").append("path")
     .attr("id", "arc")
-    .attr("d", circlePath(100, 100, 100, 0, 90));
+    .attr("d", circlePath(300, 300, 100, 0, 120));
 
 var thing = svg.append("g")
     .attr("id", "thing")
@@ -23,7 +65,7 @@ thing.append("use")
     .style("fill", "none");
 
   function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+    var angleInRadians = ((angleInDegrees - 150) * Math.PI) / 180.0;
     return {
       x: centerX + radius * Math.cos(angleInRadians),
       y: centerY + radius * Math.sin(angleInRadians),
@@ -31,9 +73,9 @@ thing.append("use")
   }
 
   function circlePath(x, y, radius, startAngle, endAngle) {
-    var start = polarToCartesian(x, y, radius, endAngle * 0.9999);
-    var end = polarToCartesian(x, y, radius, startAngle);
+    var start = polarToCartesian(x, y, radius, startAngle );
+    var end = polarToCartesian(x, y, radius, endAngle );
     var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
-    var d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y];
+    var d = ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 1, end.x, end.y];
     return d.join(' ');
   }
