4f6097f888336dfeb54a40f049fc7b7ce0ce9039
Ivan Driuk <idriuk@zaraffasoft.com>
2019-10-02 10:08:48 +0300

    mouse events and coordinates

--- range/index.js
+++ range/index.js
@@ -1,14 +1,51 @@
-const Range = () => {
+class Range extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <svg
-      width={400}
-      height={400}
-      style={{background: 'indigo'}}
-    >
-      <circle cx={200} cy={200} r={10} fill="white"></circle>
-    </svg>
-  )
+    this.state = {
+      dragging: false
+    }
+
+    this.svgRef = React.createRef()
+  }
+
+  handleMouseMove = (e) => {
+    const { svgRef, state: { dragging } } = this
+
+    if (dragging) {
+      const rect = svgRef.current.getBoundingClientRect()
+      let x = Math.round(e.pageX - rect.left)
+      let y = Math.round(e.pageY - rect.top)
+
+      console.log(x, y)
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className='App'
+        onMouseUp={(e) => { this.setState({dragging: false}) }}
+      >
+        <svg
+          width={400}
+          height={400}
+          style={{background: 'indigo'}}
+          ref={this.svgRef}
+          onMouseMove={this.handleMouseMove}
+        >
+          <circle 
+            cx={200} 
+            cy={200} 
+            r={10} 
+            fill="white"
+            onMouseDown={(e) => { this.setState({dragging: true}) }}
+          ></circle>
+        </svg>
+      </div>
+    )
+  }
+  
 }
 
 ReactDOM.render(
